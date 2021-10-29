6817d1693e8349d2b11d552bed5f89739ecdca13
Ivan Atanasov <72401635+atanasovetr@users.noreply.github.com>
2021-10-29 17:40:37 +0300

    Solution uploaded

---
+++ LimeAcademyProblem/Solution.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "./Ownable.sol";
+
+contract BookLibrary is Ownable{
+    
+    struct Book {
+        string title;
+        uint16 copies;
+    }
+    
+    mapping(uint => address[]) public bookToBorrowers;
+    mapping(address => uint[]) public userToCurrentBorrows;
+    
+    modifier containsBook(string memory _title, Book[] memory _library) {
+        bool _contains;
+        for(uint i = 0; i < _library.length; i++){
+            if(keccak256(abi.encodePacked(_library[i].title)) == keccak256(abi.encodePacked((_title)))){
+                _contains = true;
+            }
+            else{
+                _contains = false;
+            }
+        }
+        require(!_contains, "The library contains that book!");
+        _;
+    }
+    
+    modifier hasCopies(uint _copies) {
+        require(_copies > 0, "Current book must have at least 1 copy!");
+        _;
+    }
+    
+    modifier alreadyBorrowed(address _user, uint _bookId) {
+        bool _isBorrowed;
+        uint[] memory _userBooks = userToCurrentBorrows[_user];
+        for(uint i = 0; i < _userBooks.length; i++){
+            if(_userBooks[i] == _bookId){
+                _isBorrowed = true;
+            }
+            else{
+                _isBorrowed = false;
+            }
+        }
+        require(!_isBorrowed, "The user has already borrowed one copy of this book!");
+        _;
+    }
+    
+    modifier hasBook(address _user, uint _bookId) {
+        bool _isBorrowed;
+        uint[] memory _userBooks = userToCurrentBorrows[_user];
+        for(uint i = 0; i < _userBooks.length; i++){
+            if(_userBooks[i] == _bookId){
+                _isBorrowed = true;
+            }
+            else{
+                _isBorrowed = false;
+            }
+        }
+        require(_isBorrowed, "The user didn't borrow a copy of this book!");
+        _;
+    }
+    
+    modifier libraryContains(uint _bookId, Book[] memory _library) {
+        require(_bookId < _library.length && _bookId >= 0, "Library doesn't have book with this ID.");
+        _;
+    }
+    
+    Book[] public books;
+    
+    function addBook(string calldata _title, uint16 _copies) external onlyOwner containsBook(_title, books) hasCopies(_copies) {
+        books.push(Book(_title, _copies));
+        uint _bookId = books.length - 1;
+        bookToBorrowers[_bookId]; 
+    } 
+    
+    function viewAllBooks() external view returns(string memory) {
+        require(books.length > 0, "There are no availale books in the library!");
+        string memory _booksString = "";
+        for(uint i = 0; i < books.length; i++) {
+            _booksString = string(abi.encodePacked(_booksString,books[i].title, ","));
+        }
+        return(_booksString);
+    }
+    
+    function borrowBookById(uint _bookId) external hasCopies(books[_bookId].copies) alreadyBorrowed(msg.sender, _bookId) libraryContains(_bookId, books) {
+        books[_bookId].copies -= 1;
+        userToCurrentBorrows[msg.sender].push(_bookId);
+        bookToBorrowers[_bookId].push(msg.sender);
+    }
+    
+    function returnBook(uint _bookId) external hasBook(msg.sender, _bookId) libraryContains(_bookId, books) {
+        books[_bookId].copies += 1;
+        userToCurrentBorrows[msg.sender].pop;
+    }
+    
+    function findBorrowersByBookId(uint _bookId) external view returns(address[] memory) {
+        return(bookToBorrowers[_bookId]);
+    }
+
+    
+}
